cc4a3be90559450d05827e2b0c9161aa84134a7c
Erik Steffl <erik@zasran.com>
2014-04-11 21:32:44 -0700

    fixes #2 autogen.sh now links README.md to README

--- autogen.sh
+++ autogen.sh
@@ -5,6 +5,11 @@ echo "Cleanup..."
 rm -Rf autom4te.cache
 rm -f missing install-sh mkinstalldirs libtool ltmain.sh
 
+if [ ! -e README ] ; then
+  echo 'linking README.md to README to make autotools happy'
+  ln -s README.md README
+fi
+
 echo "Generating configure files... may take a while."
 
 autoreconf --install --force && \
